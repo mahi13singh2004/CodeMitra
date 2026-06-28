da0caee7d4aa3813ab44c40343363769852050c5
mahi13singh2004 <mahi13singh2004@gmail.com>
2026-06-29 01:15:40 +0530

    server work even if redis fail

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,32 @@
-import dotenv from "dotenv"
-import app from "./app.js"
-import logger from "./utils/logger.js"
-import connectDB from "./db/connectDB.js"
-import redisClient from "./utils/redis.js"
+import dotenv from "dotenv";
+import app from "./app.js";
+import logger from "./utils/logger.js";
+import connectDB from "./db/connectDB.js";
+import redisClient from "./utils/redis.js";
 
-dotenv.config()
+dotenv.config();
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
     try {
-        await connectDB()
-        await redisClient.connect()
+        await connectDB();
+
+        try {
+            await redisClient.connect();
+            logger.info("Redis Connected");
+        } catch (error) {
+            logger.warn("Redis connection failed. Continuing without Redis.");
+        }
 
         app.listen(PORT, () => {
-            logger.info(`Server is running on ${PORT}`)
-        })
-    }
-    catch (error) {
-        logger.error(error.message)
-    }
-}
+            logger.info(`Server is running on ${PORT}`);
+        });
 
-startServer()
+    } catch (error) {
+        logger.error(error.message);
+        process.exit(1);
+    }
+};
+
+startServer();
