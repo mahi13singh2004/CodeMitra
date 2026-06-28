a7fa8bd6f5b142c20b4156da931d8c7a13fbbe57
mahi13singh2004 <mahi13singh2004@gmail.com>
2026-06-29 01:01:50 +0530

    added redis url

--- backend/utils/redis.js
+++ backend/utils/redis.js
@@ -1,16 +1,16 @@
-import {createClient} from "redis"
-import logger from "../utils/logger.js"
+import { createClient } from "redis";
+import logger from "../utils/logger.js";
 
-const redisClient=createClient({
-    url:"redis://localhost:6379"
-})
+const redisClient = createClient({
+    url: process.env.REDIS_URL || "redis://localhost:6379",
+});
 
-redisClient.on("connect",()=>{
-    logger.info("Redis connected")
-})
+redisClient.on("connect", () => {
+    logger.info("Redis Connected");
+});
 
-redisClient.on("error",(error)=>{
-    logger.error(error)
-})
+redisClient.on("error", (error) => {
+    logger.error(error);
+});
 
-export default redisClient
+export default redisClient;
